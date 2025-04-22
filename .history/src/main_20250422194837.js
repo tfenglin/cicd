24c488fa4e31e81e246b97b45026da52196f3c77
枫林月下 <feng@76423.com>
2025-04-23 00:38:37 +0800

    retry

--- .history/src/main_20250422194837.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Vuex from 'vuex';
-import App from './App.vue'
-
-Vue.config.productionTip = false
-const hello = "world";
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
